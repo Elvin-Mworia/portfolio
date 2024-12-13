da07a136cb4f5b0eadc5a129f3fe703185ec0976
Elvin-Mworia <elvinmworia@gmail.com>
2024-12-13 11:28:49 +0300

    removed the footprint paw icon

--- components/logo.js
+++ components/logo.js
@@ -26,7 +26,6 @@ const Logo = () => {
     <Link href="/" scroll={false}>
       <a>
         <LogoBox>
-          <FootprintIcon />
           <Text
             color={useColorModeValue('gray.800', 'whiteAlpha.900')}
             fontFamily='M PLUS Rounded 1c", sans-serif'
