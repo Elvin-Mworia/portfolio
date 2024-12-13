aabc8408c582cb31d994d92b9c3a639a1394706b
Elvin-Mworia <elvinmworia@gmail.com>
2024-12-13 13:37:35 +0300

    removed the craftdog spinner

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -17,25 +17,22 @@ const Main = ({ children, router }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Elvin's homepage" />
         <meta name="author" content="Elvin Mworia" />
-        <meta name="author" content="craftzdog" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="twitter:title" content="Elvin Mworia" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
         <meta property="og:site_name" content="Elvin Mworia" />
         <meta name="og:title" content="Elvin Mworia" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
+        
         <title>Elvin Mworia - Homepage</title>
+        
       </Head>
 
       <NavBar path={router.asPath} />
 
       <Container maxW="container.md" pt={14}>
-        <LazyVoxelDog />
+        {/* <LazyVoxelDog /> */}
 
         {children}
 
