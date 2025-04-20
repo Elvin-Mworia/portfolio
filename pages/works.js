5489fbef5d181d831dc47e5e21a9c674d21014de
Elvin-Mworia <elvinmworia@gmail.com>
2025-04-20 12:21:09 +0300

    added Veridoc to the list of project page

--- pages/works.js
+++ pages/works.js
@@ -6,80 +6,11 @@ import { WorkGridItem } from '../components/grid-item'
 import thumbhulu from '../public/images/works/huluthumb.png'
 import thumbwhatsapp from '../public/images/works/whatsapp.png'
 import thumbhastle from '../public/images/works/hastle.png'
+import thumbveridoc from '../public/images/works/veridoc.png'
 const Works = () => (
   <Layout title="Works">
     <Container>
-      {/* <Heading as="h3" fontSize={20} mb={4}>
-        Works
-      </Heading> */}
-
-      {/* <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section>
-          <WorkGridItem id="inkdrop" title="Inkdrop" thumbnail={thumbInkdrop}>
-            A Markdown note-taking app with 100+ plugins, cross-platform and
-            encrypted data sync support
-          </WorkGridItem>
-        </Section>
-        <Section>
-          <WorkGridItem
-            id="walknote"
-            title="walknote"
-            thumbnail={thumbWalknote}
-          >
-            Music recommendation app for iOS
-          </WorkGridItem>
-        </Section>
-
         <Section delay={0.1}>
-          <WorkGridItem
-            id="fourpainters"
-            title="The four painters"
-            thumbnail={thumbFourPainters}
-          >
-            A video work generated with deep learning, imitating famous four
-            painters like Van Gogh
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.1}>
-          <WorkGridItem id="menkiki" thumbnail={thumbMenkiki} title="Menkiki">
-            An app that suggests ramen(noodle) shops based on a given photo of
-            the ramen you want to eat
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid> */}
-
-      {/* <Section delay={0.2}>
-        <Divider my={6} />
-
-        <Heading as="h3" fontSize={20} mb={4}>
-          Collaborations
-        </Heading>
-      </Section> */}
-
-      {/* <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay={0.3}>
-          <WorkGridItem id="margelo" thumbnail={thumbMargelo} title="Margelo">
-            A website of the elite app development and contracting agency based
-            in Austria
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.3}>
-          <WorkGridItem
-            id="modetokyo"
-            thumbnail={thumbModeTokyo}
-            title="mode.tokyo"
-          >
-            The mode magazine for understanding to personally enjoy Japan
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.3}>
-          <WorkGridItem id="styly" thumbnail={thumbStyly} title="Styly">
-            A VR Creative tools for fashion brands
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid> */}
-
-      <Section delay={0.1}>
         <Divider my={6} />
 
         <Heading as="h3" fontSize={20} mb={4}>
@@ -109,6 +40,15 @@ const Works = () => (
             title="Hastle web app"
           >
             A web app that makes searching of local manual jobs easier
+          </WorkGridItem>
+        </Section>
+        <Section delay={1.0}>
+          <WorkGridItem
+            id="veridoc"
+            thumbnail={thumbveridoc}
+            title="Veridoc"
+          >
+            Blockchain based e-filling system 
           </WorkGridItem>
         </Section>
       </SimpleGrid>
